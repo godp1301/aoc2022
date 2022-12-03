089d4f8966834a8fd433973b5393032798a0b3b0
Philippe Godin <pgodin@spypoint.com>
2022-12-02 20:43:41 -0500

    Day2 - part 2

--- day2/test_rock_paper_scissors.py
+++ day2/test_rock_paper_scissors.py
@@ -1,26 +1,23 @@
 from day2.rock_paper_scissors import game, calculate
 
 
-def test_rock_beats_scissors():
-    assert calculate('A Z') == 3
-    assert calculate('C X') == 7
-
-
-def test_paper_beats_rock():
+def test_opponent_has_to_win():
+    assert calculate('A X') == 3
     assert calculate('B X') == 1
-    assert calculate('A Y') == 8
+    assert calculate('C X') == 2
 
 
-def test_scissors_beats_paper():
-    assert calculate('C Y') == 2
+def test_opponent_has_to_loose():
+    assert calculate('A Z') == 8
     assert calculate('B Z') == 9
+    assert calculate('C Z') == 7
 
 
 def test_draws():
-    assert calculate('A X') == 4
+    assert calculate('A Y') == 4
     assert calculate('B Y') == 5
-    assert calculate('C Z') == 6
+    assert calculate('C Y') == 6
 
 
 def test_example():
-    assert game('example.txt') == 15
+    assert game('example.txt') == 12
