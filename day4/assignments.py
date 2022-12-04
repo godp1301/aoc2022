9d88c0fd99dab2472db5cfb7393c2f8b01f046ef
Philippe Godin <pgodin@spypoint.com>
2022-12-04 17:19:25 -0500

    Day4 - part 2

--- day4/assignments.py
+++ day4/assignments.py
@@ -1,21 +1,25 @@
-def is_contained_in(assignment):
+def overlaps(assignment):
     pairs = assignment.split(',')
     fp_numbers = pairs[0].split('-')
     fp_lower_bound, fp_upper_bound = int(fp_numbers[0]), int(fp_numbers[1])
     sp_numbers = pairs[1].split('-')
     sp_lower_bound, sp_upper_bound = int(sp_numbers[0]), int(sp_numbers[1])
 
-    if fp_lower_bound >= sp_lower_bound and fp_upper_bound <= sp_upper_bound \
-            or sp_lower_bound >= fp_lower_bound and sp_upper_bound <= fp_upper_bound:
+    if fp_lower_bound in range(sp_lower_bound, sp_upper_bound + 1) \
+            or fp_upper_bound in range(sp_lower_bound, sp_upper_bound + 1):
+        return True
+
+    if sp_lower_bound in range(fp_lower_bound, fp_upper_bound + 1) \
+            or sp_upper_bound in range(fp_lower_bound, fp_upper_bound + 1):
         return True
 
     return False
 
 
-def count_pairs_contained_in_others(puzzle):
+def count_pairs_overlapping_others(puzzle):
     sum = 0
     for line in puzzle:
-        sum += 1 if is_contained_in(line) else 0
+        sum += 1 if overlaps(line) else 0
 
     return sum
 
@@ -23,4 +27,4 @@ def count_pairs_contained_in_others(puzzle):
 if __name__ == '__main__':
     with open('input.txt', 'r') as file:
         puzzle = [line[:-1] for line in file.readlines()]
-    print(count_pairs_contained_in_others(puzzle))
+    print(count_pairs_overlapping_others(puzzle))
--- day4/test_assignments.py
+++ day4/test_assignments.py
@@ -1,20 +1,24 @@
-from day4.assignments import is_contained_in, count_pairs_contained_in_others
+from day4.assignments import count_pairs_overlapping_others, overlaps
 
 
-def test_not_contained():
-    assert is_contained_in('50-52,32-51') is False
+def test_not_overlapping():
+    assert overlaps('2-4,6-8') is False
 
 
-def test_first_part_contained_in_second():
-    assert is_contained_in('12-18,10-44') is True
+def test_first_part_overlaps():
+    assert overlaps('5-7,7-9') is True
 
 
-def test_second_part_contained_in_first():
-    assert is_contained_in('22-28,23-24') is True
+def test_overlaps():
+    assert overlaps('6-6,4-6') is True
+
+
+def test_second_part_overlaps():
+    assert overlaps('7-9,5-7') is True
 
 
 def test_example():
     with open('example.txt', 'r') as file:
         puzzle = [line[:-1] for line in file.readlines()]
 
-    assert count_pairs_contained_in_others(puzzle) == 2
+    assert count_pairs_overlapping_others(puzzle) == 4
