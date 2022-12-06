897d7943b1e039b31936af92693168fd5fdbca06
Philippe Godin <pgodin@spypoint.com>
2022-12-06 00:27:17 -0500

    Day6 - part2

--- day6/device.py
+++ day6/device.py
@@ -4,8 +4,15 @@ def find_marker(signal):
             return i + 4
 
 
+def find_som_marker(signal):
+    for i, _ in enumerate(signal):
+        if len(set(signal[i:i + 14])) == 14:
+            return i + 14
+
+
 if __name__ == '__main__':
     with open('input.txt', 'r') as file:
         puzzle = file.readlines()
 
     print(find_marker(puzzle[0]))
+    print(find_som_marker(puzzle[0]))
--- day6/test_device.py
+++ day6/test_device.py
@@ -1,4 +1,4 @@
-from day6.device import find_marker
+from day6.device import find_marker, find_som_marker
 
 
 def test_first_example():
@@ -11,3 +11,7 @@ def test_second_example():
 
 def test_third_example():
     assert find_marker('nppdvjthqldpwncqszvftbrmjlhg') == 6
+
+
+def test_first_som_marker():
+    assert find_som_marker('mjqjpqmgbljsphdztnvjfqwrcgsmlb') == 19
