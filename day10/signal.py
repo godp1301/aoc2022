af7b852baa8a711d08836db39c622a9df419cef7
Philippe Godin <pgodin@spypoint.com>
2022-12-10 01:48:59 -0500

    Day10 - part2

--- day10/signal.py
+++ day10/signal.py
@@ -1,10 +1,7 @@
 from collections import deque
 
 register = []
-
-
-def get_strength():
-    return 1
+sprite_pos = 1
 
 
 def compute(program):
@@ -38,3 +35,19 @@ def compute(program):
 def get_signal_strength():
     global register
     return register[19] * 20 + register[59] * 60 + register[99] * 100 + register[139] * 140 + register[179] * 180 + register[219] * 220
+
+
+def display(computed):
+    sprite = 1
+    to_display = deque(computed)
+    for i in range(6):
+        pixels = ''
+        for j in range(40):
+            pixels += '#' if sprite - 1 <= j <= sprite + 1 else '.'
+            sprite = to_display.popleft()
+        print(pixels)
+
+
+
+
+
--- day10/test_signal.py
+++ day10/test_signal.py
@@ -1,12 +1,24 @@
-from day10.signal import get_strength, compute, get_signal_strength
-
-
-def test_initial_signal_strength():
-    assert get_strength() == 1
+from day10.signal import compute, get_signal_strength, display
 
 
 def test_simple_example():
     assert compute(['noop', 'addx 3', 'addx -5']) == [1, 1, 4, 4, -1]
+
+
+def test_complex_example_display():
+    with open('example.txt', 'r') as file:
+        program = [line[:-1] for line in file.readlines()]
+
+    computed = compute(program)
+    display(computed)
+
+
+def test_input_display():
+    with open('input.txt', 'r') as file:
+        program = [line[:-1] for line in file.readlines()]
+
+    computed = compute(program)
+    display(computed)
 
 
 def test_input():
